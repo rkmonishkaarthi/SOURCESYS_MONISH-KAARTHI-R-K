1f7ad6239d30d45ec61b46e9742b28e78162fa7e
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-02 22:36:41 +0530

    Weighted Performance

--- Mini Project.py
+++ Mini Project.py
@@ -12,15 +12,20 @@ print("\nAvg Sales per Employee:\n",avg)
 month_avg = np.mean(sales, axis=0)
 print("\nAvg Sales per Month:\n", month_avg)
 
-best_employee = np.argmax(employee_avg)
+best_employee = np.argmax(avg)
 print("\nBest Employee :", best_employee)
 
 employee_std = np.std(sales, axis=1)
 print("\nEmployee Sales Std:\n", employee_std)
 
-low = np.where(employee_avg < 40)
+low = np.where(avg < 40)
 print("\nEmployees Below Target:\n", low)
 
-ranking = np.argsort(employee_avg)
-top3 = ranking[-3:]
+ranking = np.argsort(avg)
+top3 = ranking[:3]
 print("\nTop 3 Employees:\n", top3)
+
+weights = np.array([0.05, 0.05, 0.08, 0.1, 0.12, 0.1, 0.1, 0.15, 0.15, 0.1])
+weighted_performance = np.dot(sales, weights)
+print("\nWeighted Performance:\n", weighted_performance)
+
