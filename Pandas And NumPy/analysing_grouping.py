e2f5378ab99b57b030ad2820aaa9830f849f4239
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-07 12:59:10 +0530

    GroupBY

--- Pandas And NumPy/analysing_grouping.py
+++ Pandas And NumPy/analysing_grouping.py
@@ -11,3 +11,16 @@ df["actual_price"] = df["actual_price"].replace('[₹,]', '', regex=True).astype
 
 df["no_of_ratings"] = df["no_of_ratings"].str.replace(',', '', regex=True)
 df["no_of_ratings"] = pd.to_numeric(df["no_of_ratings"], errors="coerce")
+
+avg_price = df.groupby("main_category")["actual_price"].mean()
+count_products = df.groupby("main_category")["name"].count()
+sum_price = df.groupby("main_category")["actual_price"].sum()
+
+print("Average price by category")
+print(avg_price)
+
+print("Number of products by category")
+print(count_products)
+
+print("Total price by category")
+print(sum_price)
