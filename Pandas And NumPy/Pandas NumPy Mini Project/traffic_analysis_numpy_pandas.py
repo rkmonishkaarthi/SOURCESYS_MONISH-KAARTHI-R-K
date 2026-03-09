beb9867f4b0e5acc7d0a9f07a6407475bb3d4b57
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-09 18:51:56 +0530

    Data Cleaning

--- Pandas And NumPy/Pandas NumPy Mini Project/traffic_analysis_numpy_pandas.py
+++ Pandas And NumPy/Pandas NumPy Mini Project/traffic_analysis_numpy_pandas.py
@@ -5,3 +5,14 @@ df = pd.read_csv("traffictwomonth.csv")
 
 print("\nDataset Preview")
 print(df.head())
+
+# Data Cleaning
+
+df["Total"] = df[["CarCount", "BikeCount", "BusCount", "TruckCount"]].sum(axis=1)
+
+df["Time"] = pd.to_datetime(df["Time"])
+
+df["Hour"] = df["Time"].dt.hour
+
+print("\nDataset After Cleaning")
+print(df.head())
