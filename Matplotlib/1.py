527e7879ded926194d380cb77f465b7b633168de
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-10 20:33:47 +0530

    Annotate

--- Matplotlib/1.py
+++ Matplotlib/1.py
@@ -8,11 +8,18 @@ plt.figure(figsize=(10,5),dpi=100)
 plt.plot(x, y1, color="blue", linestyle="--", linewidth=2, marker="s", markersize=6,label='Product A')
 plt.plot(x, y2, color="orange", linestyle="dotted", linewidth=2, marker="d", markersize=6,label='Product B')
 
+plt.annotate(
+    "Highest",
+    xy=("May",220),
+    xytext=("Apr",190),
+    arrowprops=dict(facecolor="green")
+)
+
 plt.xlabel("Months")
 plt.ylabel("Sales")
 plt.title("Sales Graph")
 plt.legend()
 # plt.savefig("salesplot.png")
-# plt.savefig("salesplot.jpg")
+plt.savefig("salesplot.jpg")
 plt.grid(True)
 plt.show()
