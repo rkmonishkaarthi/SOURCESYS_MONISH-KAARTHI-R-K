1d02fe6ea1aca14642771d3181ecf2fb8e0d9493
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-10 20:10:38 +0530

    Added fig size and dpi

--- Matplotlib/1.py
+++ Matplotlib/1.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 x = ["Jan","Feb","Mar","Apr","May"]
 y1 = [120, 150, 170, 200, 220]   # Product A sales
 y2 = [100, 130, 160, 180, 210]   # Product B sales
-
+plt.figure(figsize=(10,5),dpi=100)
 plt.plot(x, y1, color="blue", linestyle="--", linewidth=2, marker="s", markersize=6,label='Product A')
 plt.plot(x, y2, color="orange", linestyle="dotted", linewidth=2, marker="d", markersize=6,label='Product B')
 
