015d290e84c86ff2cdbff483f5dbbb996f0614e7
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-11 20:57:24 +0530

    Bar Plot

--- Matplotlib/Plotting/Plotting.py
+++ Matplotlib/Plotting/Plotting.py
@@ -1,12 +1,23 @@
 import matplotlib.pyplot as plt
 
 # Scatter Plot 
-hours = [3,2,4,1,2,5]
-marks = [75,60,80,35,40,90]
+# hours = [3,2,4,1,2,5]
+# marks = [75,60,80,35,40,90]
 
-plt.scatter(hours,marks)
-plt.xlabel("my study hours")
-plt.ylabel("my marks per subject")
-plt.title("scatter plot")
-plt.savefig("scatterplot1.png")
+# plt.scatter(hours,marks)
+# plt.xlabel("my study hours")
+# plt.ylabel("my marks per subject")
+# plt.title("scatter plot")
+# plt.savefig("scatterplot1.png")
+# plt.show()
+
+# Bar Plot
+products = ["A","B","C","D","E"]
+sales = [70,50,40,90,30]
+
+plt.bar(products,sales)
+plt.xlabel("products")
+plt.ylabel("sales")
+plt.title("product sales")
+plt.savefig("barplot1.png")
 plt.show()
