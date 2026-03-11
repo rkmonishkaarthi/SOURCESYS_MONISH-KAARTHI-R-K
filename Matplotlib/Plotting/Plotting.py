e75689090a76ba07312bf088046744e8e2f23473
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-11 21:13:08 +0530

    Output 2

--- Matplotlib/Plotting/Plotting.py
+++ Matplotlib/Plotting/Plotting.py
@@ -1,40 +1,40 @@
 import matplotlib.pyplot as plt
 
 # Scatter Plot 
-# hours = [3,2,4,1,2,5]
-# marks = [75,60,80,35,40,90]
+hours = [1,2,3,4,5,6]
+marks = [40,50,60,70,85,90]
 
-# plt.scatter(hours,marks)
-# plt.xlabel("my study hours")
-# plt.ylabel("my marks per subject")
-# plt.title("scatter plot")
-# plt.savefig("scatterplot1.png")
-# plt.show()
+plt.scatter(hours,marks)
+plt.xlabel("my study hours")
+plt.ylabel("my marks per subject")
+plt.title("scatter plot")
+plt.savefig("scatterplot2.png")
+plt.show()
 
-# # Bar Plot
-# products = ["A","B","C","D","E"]
-# sales = [70,50,40,90,30]
+# Bar Plot
+products = ["Pen","Book","Bag","Bottle","Scale"]
+sales = [100,150,80,120,60]
 
-# plt.bar(products,sales)
-# plt.xlabel("products")
-# plt.ylabel("sales")
-# plt.title("product sales")
-# plt.savefig("barplot1.png")
-# plt.show()
+plt.bar(products,sales)
+plt.xlabel("products")
+plt.ylabel("sales")
+plt.title("product sales")
+plt.savefig("barplot2.png")
+plt.show()
 
 # Histogram
-# marks = [60,42,67,78,89,92,45,32,42,83,85]
+marks = [55,60,65,70,75,80,85,90,95,50]
 
-# plt.hist(marks)
-# plt.xlabel("marks")
-# plt.ylabel("frequency")
-# plt.title("Marks Distribution")
-# plt.savefig("histogram.png")
-# plt.show()
+plt.hist(marks)
+plt.xlabel("marks")
+plt.ylabel("frequency")
+plt.title("Marks Distribution")
+plt.savefig("histogram2.png")
+plt.show()
 
 # Pie Chart
 mobiles = ["Apple","Samsung","Realme","Vivo","Oppo","Others"]
-price = [60000,40000,25000,28000,32000,70000]
+price = [50000,45000,20000,25000,30000,15000]
 
 plt.pie(price, labels=mobiles, autopct="%1.1f%%", startangle=90)
 plt.title("Share")
