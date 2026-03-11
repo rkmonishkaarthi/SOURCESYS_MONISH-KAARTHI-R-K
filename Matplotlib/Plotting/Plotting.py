a345489caff356af567dc625171cd349baa854c7
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-11 21:21:46 +0530

    Output 4

--- Matplotlib/Plotting/Plotting.py
+++ Matplotlib/Plotting/Plotting.py
@@ -2,42 +2,42 @@ import matplotlib.pyplot as plt
 
 # Scatter Plot 
 hours = [1,2,3,4,5,6]
-marks = [200,300,350,420,480,520]
+marks = [500,700,900,1100,1300,1500]
 
 plt.scatter(hours,marks)
 plt.xlabel("my study hours")
 plt.ylabel("my marks per subject")
 plt.title("scatter plot")
-plt.savefig("scatterplot3.png")
+plt.savefig("scatterplot4.png")
 plt.show()
 
 # Bar Plot
-products = ["Protein","Creatine","Gloves","Shoes","Bottle"]
-sales = [40,30,50,20,60]
+products = ["Laptop","Mouse","Keyboard","Monitor","Speaker"]
+sales = [200,500,350,150,250]
 
 plt.bar(products,sales)
 plt.xlabel("products")
 plt.ylabel("sales")
 plt.title("product sales")
-plt.savefig("barplot3.png")
+plt.savefig("barplot4.png")
 plt.show()
 
 # Histogram
-marks = [180,200,220,250,260,300,320,340,360]
+marks = [450,500,600,700,800,900,1000,1100]
 
 plt.hist(marks)
 plt.xlabel("marks")
 plt.ylabel("frequency")
 plt.title("Marks Distribution")
-plt.savefig("histogram3.png")
+plt.savefig("histogram4.png")
 plt.show()
 
 # Pie Chart
-Product = ["Cardio","Weight","Yoga","Crossfit","Zumba","Other"]
-price = [25,20,15,18,12,10]
+mobiles = ["North","South","East","West","Online","Retail"]
+price = [30,20,15,10,15,10]
 
-plt.pie(price, labels=Product, autopct="%1.1f%%", startangle=90)
+plt.pie(price, labels=mobiles, autopct="%1.1f%%", startangle=90)
 plt.title("Share")
-plt.savefig("piechart3.png")
+plt.savefig("piechart4.png")
 plt.axis("equal")
 plt.show()
