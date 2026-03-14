c0179385bb8664cf280023a6738a08da3c6c2894
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-14 14:41:54 +0530

    Data Cleaning

--- public_transport_analysis/analysis.py
+++ public_transport_analysis/analysis.py
@@ -6,3 +6,23 @@ data = pd.read_csv("MTA.csv")
 
 print("Original Dataset")
 print(data.head())
+
+# Data cleaning
+data = data[[
+    "Date",
+    "Subways: Total Estimated Ridership",
+    "Buses: Total Estimated Ridership",
+    "LIRR: Total Estimated Ridership",
+    "Metro-North: Total Estimated Ridership"
+]]
+
+data.columns = ["Date", "Subways", "Buses", "LIRR", "MetroNorth"]
+
+data["Date"] = pd.to_datetime(data["Date"])
+
+data = data.dropna()
+
+data = data.head(50)
+
+print("\nCleaned Dataset")
+print(data.head())
