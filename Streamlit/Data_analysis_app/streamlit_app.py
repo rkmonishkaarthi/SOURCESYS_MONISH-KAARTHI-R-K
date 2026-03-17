29dc300175c74605aa6cd10256b3749f82783c7b
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-17 22:56:58 +0530

    Added data cleaning and preprocessing

--- Streamlit/Data_analysis_app/streamlit_app.py
+++ Streamlit/Data_analysis_app/streamlit_app.py
@@ -8,3 +8,20 @@ uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
     st.write(data.head())
+
+data = data[[
+    "Date",
+    "Subways: Total Estimated Ridership",
+    "Buses: Total Estimated Ridership",
+    "LIRR: Total Estimated Ridership",
+    "Metro-North: Total Estimated Ridership"
+]]
+
+data.columns = ["Date", "Subways", "Buses", "LIRR", "MetroNorth"]
+
+data["Date"] = pd.to_datetime(data["Date"])
+data = data.dropna()
+data = data.head(50)
+
+st.write("Cleaned Data")
+st.write(data.head())
