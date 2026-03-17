43a237c88773e29d7b2fbecb1c906527a29a0869
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-17 22:49:57 +0530

    Read and display uploaded dataset

--- Streamlit/Data_analysis_app/streamlit_app.py
+++ Streamlit/Data_analysis_app/streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Public Transport Analysis App")
 
 uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
 
 if uploaded_file is not None:
-    st.success("File uploaded successfully")
+    data = pd.read_csv(uploaded_file)
+    st.write(data.head())
